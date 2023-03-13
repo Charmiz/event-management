54fc6d959ae38ab20ec629e7da4ecb51375ad6b8
Charmi Zala <31683538+Charmiz@users.noreply.github.com>
2023-03-13 15:18:23 +0530

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -3,7 +3,7 @@
 $servername = "ems-app-server.mysql.database.azure.com";
 $username = "wnpanrdwat";
 $password = "A7R5357CQ7C6017Q$";
-$db = "ems-app-database";
+$database = "ems-app-database";
 
 // Create connection
 $db = mysqli_init();
@@ -13,7 +13,7 @@ $db->ssl_set(
     '/etc/mysql/ssl/ca-cert.pem',
     NULL,
     NULL);
-$con = mysqli_real_connect($db,$servername, $username, $password,'db',3306,NULL,MYSQLI_CLIENT_SSL);
+$con = mysqli_real_connect($servername, $username, $password,$database);
 
 // Check connection
 if (!$con) {
