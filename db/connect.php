eae7f50c12c74e30c24055299ee6927ef5cb1495
Charmi Zala <31683538+Charmiz@users.noreply.github.com>
2023-03-15 16:53:06 +0530

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,16 @@
 <?php
-
+/*
 $servername = "ems-app-server.mysql.database.azure.com";
 $username = "wnpanrdwat";
 $password = "A7R5357CQ7C6017Q$";
 $database = "ems-app-database";
+*/
+
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DB_NAME'];
+
 
 $con = mysqli_connect($servername, $username, $password,$database);
 
