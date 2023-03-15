cd3add58ad221dfe334d79428b4ad6fbbb633ef3
Charmi Zala <31683538+Charmiz@users.noreply.github.com>
2023-03-15 17:06:32 +0530

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -6,6 +6,9 @@ $password = "A7R5357CQ7C6017Q$";
 $database = "ems-app-database";
 */
 
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 $servername = $_SERVER['DB_HOST'];
 $username = $_SERVER['DB_USERNAME'];
 $password = $_SERVER['DB_PASSWORD'];
