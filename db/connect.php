3d226c6abebc28a5d34ec312bb8cab30778c06d3
Charmi Zala <31683538+Charmiz@users.noreply.github.com>
2023-03-15 17:26:02 +0530

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -5,14 +5,15 @@ $username = "wnpanrdwat";
 $password = "A7R5357CQ7C6017Q$";
 $database = "ems-app-database";
 */
+require 'vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-$servername = $_SERVER['DB_HOST'];
-$username = $_SERVER['DB_USERNAME'];
-$password = $_SERVER['DB_PASSWORD'];
-$database = $_SERVER['DB_DATABASE '];
+$servername = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$database = $_ENV['DB_DATABASE '];
 
 
 $con = mysqli_connect($servername, $username, $password,$database);
